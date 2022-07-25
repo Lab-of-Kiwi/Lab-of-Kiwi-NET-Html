430e8d098b1867866141a601c51219a24961904e
Steven Johnson <sjohnson0684@gmail.com>
2022-07-25 15:48:49 -0400

    HtmlDocument load methods.

--- src/LabOfKiwi.Html/HtmlDocument.cs
+++ src/LabOfKiwi.Html/HtmlDocument.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
 using System.Xml;
 
 namespace LabOfKiwi.Html;
@@ -41,6 +43,67 @@ public sealed partial class HtmlDocument : HtmlNode
         using var writer = new StringWriter();
         CoreDocument.Save(writer);
         return writer.ToString();
+    }
+
+    public static HtmlDocument Load(Stream stream)
+    {
+        return Load(new StreamReader(stream));
+    }
+
+    public static HtmlDocument Load(Stream stream, bool detectEncodingFromByteOrderMarks)
+    {
+        return Load(new StreamReader(stream, detectEncodingFromByteOrderMarks));
+    }
+
+    public static HtmlDocument Load(Stream stream, Encoding encoding)
+    {
+        return Load(new StreamReader(stream, encoding));
+    }
+
+    public static HtmlDocument Load(Stream stream, Encoding encoding, bool detectEncodingFromByteOrderMarks)
+    {
+        return Load(new StreamReader(stream, encoding, detectEncodingFromByteOrderMarks));
+    }
+
+    public static HtmlDocument Load(Stream stream, Encoding encoding, bool detectEncodingFromByteOrderMarks, int buffersize)
+    {
+        return Load(new StreamReader(stream, encoding, detectEncodingFromByteOrderMarks, buffersize));
+    }
+
+    public static HtmlDocument Load(string path)
+    {
+        if (path == null)
+        {
+            throw new ArgumentNullException(nameof(path));
+        }
+
+        var coreDocument = new HtmlAgilityPack.HtmlDocument();
+        coreDocument.Load(path);
+        return new HtmlDocument(coreDocument);
+    }
+
+    public static HtmlDocument Load(TextReader reader)
+    {
+        if (reader == null)
+        {
+            throw new ArgumentNullException(nameof(reader));
+        }
+
+        var coreDocument = new HtmlAgilityPack.HtmlDocument();
+        coreDocument.Load(reader);
+        return new HtmlDocument(coreDocument);
+    }
+
+    public static HtmlDocument LoadHtml(string html)
+    {
+        if (html == null)
+        {
+            throw new ArgumentNullException(nameof(html));
+        }
+
+        var coreDocument = new HtmlAgilityPack.HtmlDocument();
+        coreDocument.LoadHtml(html);
+        return new HtmlDocument(coreDocument);
     }
 
     internal HtmlAgilityPack.HtmlDocument CoreDocument { get; }
